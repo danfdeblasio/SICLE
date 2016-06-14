94fa8b7b40854ea66cfda6208e5a49a674b7c325
Dan DeBlasio <dan@dandeblasio.com>
2016-06-14 14:19:53 -0700

    Updating the README

--- Make_SiClE_package.pl
+++ Make_SiClE_package.pl
@@ -3,9 +3,10 @@ use strict;
 
 my $rc_name = shift;
 
-system("mkdir Releases/SiClE_$rc_name");
+system("mkdir -p Releases/SiClE_$rc_name/examples");
 open MAKEFILE,">Releases/SiClE_$rc_name/Makefile";
-system("cp groupfile.txt en.lproj/Phylogeny.cpp en.lproj/Phylogeny.h SisterFinder/main.cpp SisterFinder/en.lproj/SiClE.cpp SisterFinder/en.lproj/SiClE.h Releases/SiClE_$rc_name");
+system("cp testing_examples/groupfile.txt en.lproj/Phylogeny.cpp en.lproj/Phylogeny.h SisterFinder/main.cpp SisterFinder/en.lproj/SiClE.cpp SisterFinder/en.lproj/SiClE.h Releases/SiClE_$rc_name");
+system("cp testing_examples/groupfile.txt testing_examples/Phy001MQP3_SALRD_new.tre Releases/SiClE_$rc_name/examples");
 print MAKEFILE "all: SiClE\n\n";
 print MAKEFILE "clean:\n\trm Phylogeny.o SiClE.o SiClE\n\n";
 print MAKEFILE "Phylogeny.o: Phylogeny.cpp Phylogeny.h\n\tg++ -std=c++0x -c Phylogeny.cpp\n\n";
@@ -45,7 +46,26 @@ Optional Arguments:
 Group File
 ----------
 An example group file can be found on the SiClE website at http://eebweb.arizona.edu/sicle/ or directly at 
-http://eebweb.arizona.edu/sicle/examples/groupFile.txt.
+http://eebweb.arizona.edu/sicle/examples/groupFile.txt or included in this package.
+
+Examples
+--------
+We have included an example tree in this file from the experements described in the SICLE publication. This tree is Phy001MQP3_SALRD_new.tre
+
+Using SICLE to search for Cyanobacteria in this tree shows that Betaproteobacteria is the only monophyletic sister clade and the minimum boostrap suport along the branch that conncts them is 0.997.
+
+./SiClE Cyanobacteria examples/Phy001MQP3_SALRD_new.tre
+	examples/Phy001MQP3_SALRD_new.tre:
+	Cyanobacteria	Betaproteobacteria	0.997
+
+If you use the provided hierarchy you find that the other sister to Cyanobacteria consists only of different Eubacteria
+
+./SiClE --groupFile groupfile.txt examples/Cyanobacteria examples/Phy001MQP3_SALRD_new.tre
+	examples/Phy001MQP3_SALRD_new.tre:
+	Cyanobacteria	Eubacteria	0.997
+	Cyanobacteria	Betaproteobacteria	0.997
+	
+The output of SICLE is tab-delimited and can easily be parsed a hand wirtten script or any available spreadsheet application like EXCEL. 
 
 Complex Runtime
 ----------------
